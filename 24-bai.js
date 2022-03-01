4e1aaf8c979977762de887613175e07f88e5f3ef
WaltaiBaltai <baijankoshmatov@mail.ru>
2022-03-01 22:22:10 +0600

    done

--- 24-bai.js
+++ 24-bai.js
@@ -1,13 +1,8 @@
 let element = document.getElementById("changeColor");
 element.addEventListener("click", myF);
 function myF() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < document.getElementsByClassName("box").length; i++) {
     document.getElementsByClassName("box")[i].style.background =
       "#" + (((1 << 24) * Math.random()) | 0).toString(16);
   }
 }
-
-/*
-Кама, салам! Вместо (i < 5 ) как можно по другому записать?
-я пробовал "document.getElementsByClassName("box").length" и т.п. и т.д., но не получилось
-*/
